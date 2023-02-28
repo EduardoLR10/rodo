dc02f8d445139f11108c4ff55e1aaf0ebfe3d885
EduardoLR10 <dudulr10@gmail.com>
2023-02-28 00:24:20 -0300

    Add command line support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod commands;
+use commands::RodoCommand;
+use commands::Cli;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let _ = args.next().expect("something went wrong");
+
+    let mut chosen_command = None;
+    
+    while let Some(arg) = args.next() {
+        match RodoCommand::from_string(arg) {
+	    RodoCommand::Catalog => chosen_command = Some(RodoCommand::Catalog)
+        }
+    }
+    match chosen_command {
+	Some(c) =>  eprintln!("{:?}", c),
+	None => RodoCommand::full_usage()
+    }	
 }
