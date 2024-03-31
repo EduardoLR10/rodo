5b9a1220eebdfca86cc596cb056bcc0f32825dfc
Edil Medeiros <jose.edil@gmail.com>
2024-03-31 11:34:14 -0300

    Add parse_todo
    The TODO parser should treat it's own errors instead of exposing them to the caller.
    The TODO parser can either
    
    1. Succeed, in which case it should report the text it found for later
    processing;
    2. Fail, in which case there's no TODO to report.

--- src/commands/parser.rs
+++ src/commands/parser.rs
@@ -7,3 +7,67 @@ pub fn todo() -> impl Fn(&[u8]) -> IResult<&[u8], &[u8], Error<&[u8]>> {
 	preceded(header, content)(i)
     }
 }
+
+#[derive(Debug, PartialEq)]
+pub enum PResult<'a> {
+    Todo {
+        text: &'a str
+    },
+    NoTodo
+}
+
+pub fn parse_todo<'a>(todo_tag: &'a str, input: &'a str) -> PResult<'a> {
+    preceded(
+        take_until(todo_tag),
+        rest::<&str, ()>
+    )(input).map_or(PResult::NoTodo,
+                    |(_, result)| PResult::Todo {text: result} )
+}
+
+#[test]
+fn todo_clean() {
+    let input = "TODO: test todo";
+    assert_eq!(parse_todo("TODO:", input), PResult::Todo{text: "TODO: test todo"});
+}
+
+#[test]
+fn todo_in_comment() {
+    let input = "// TODO: test todo";
+    assert_eq!(parse_todo("TODO:", input), PResult::Todo{text: "TODO: test todo"});
+}
+
+#[test]
+fn todo_different_tag() {
+    let input = "FIXME: different tag";
+    assert_eq!(parse_todo("TODO:", input), PResult::NoTodo);
+}
+
+
+
+pub fn parse_todo1<'a>(todo_tag: &'a str, input: &'a str) -> IResult<&'a str, &'a str> {
+    preceded(
+        take_until(todo_tag),
+        rest
+    )(input)
+}
+
+#[test]
+fn todo_clean1() {
+    let input = "TODO: test todo";
+    assert_eq!(parse_todo1("TODO:", input), Ok(("", "TODO: test todo")));
+}
+
+#[test]
+fn todo_in_comment1() {
+    let input = "// TODO: test todo";
+    assert_eq!(parse_todo1("TODO:", input), Ok(("", "TODO: test todo")));
+}
+
+#[test]
+fn todo_different_tag1() {
+    let input = "FIXME: different tag";
+    match parse_todo1("TODO:", input) {
+        Ok(_) => assert!(false),
+        Err(_) => assert!(true)
+    }
+}
