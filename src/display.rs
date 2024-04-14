617bf2a063a01cc8aa5d1db4a95f3be679659321
Edil Medeiros <jose.edil@gmail.com>
2024-04-14 16:34:04 -0300

    Change Todo displaying functions
    Because Todos now carry owned strings that can be processed.

--- src/display.rs
+++ src/display.rs
@@ -1,16 +1,18 @@
 use colored::Colorize;
 
-fn display_todo(msg: &mut String) {
-    msg.replace_range(..5, "");
-    let body = msg.trim();
-    println!(
+use crate::commands::parser::Todo;
+
+impl Todo {
+    pub fn display(todo: Todo) -> String {
+    format!(
         "{} {}",
-        "TODO:".bold().blue().underline(),
-	body)
+        todo.tag.bold().blue().underline(),
+	      todo.text)
+    }
 }
 
-pub fn display_todos(msgs: Vec<String>) {
-    for mut msg in msgs {
-	display_todo(&mut msg);
+pub fn display_todos(todos: Vec<Todo>) {
+    for todo in todos {
+	      println!("{}", Todo::display(todo));
     }
 }
