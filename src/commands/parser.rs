ba9b2703309f0445f907e550b3feb962c5f5dfa8
Edil Medeiros <jose.edil@gmail.com>
2024-04-14 16:43:11 -0300

    Move unit tests to test module
    As recommended in the rust book
    (https://doc.rust-lang.org/book/ch11-03-test-organization.html#unit-tests)

--- src/commands/parser.rs
+++ src/commands/parser.rs
@@ -12,7 +12,6 @@ pub struct Todo {
     pub text: String,
 }
 
-
 fn parse_todo<'a>(todo_tag: &'a str, input: &'a str) -> IResult<&'a str, Todo> {
     // remove everything prior to the tag
     let (input, _) = take_until(todo_tag)(input)?;
@@ -42,150 +41,156 @@ pub fn parse_file(file_content: &str) -> Vec<Todo> {
     }
 }
 
-#[test]
-fn todos() {
-    let input =
-        "line 1\n\
-         TODO: todo1\n\
-         line 2\n\
-         line 3 -- TODO: todo2\n";
 
-    assert_eq!(parse_todos(input),
-               Ok(("",
-                   vec![
-                       Todo{ tag: "TODO:".to_owned(),
-                             text: " todo1".to_owned()},
-                       Todo{ tag: "TODO:".to_owned(),
-                             text: " todo2".to_owned()},
-                   ])
-               )
-    );
+#[cfg(test)]
+mod test {
+    use crate::commands::parser::{parse_todos, Todo, parse_todo};
 
-    let input = "line 1\n";
-    assert_eq!(parse_todos(input),
-               Ok(("line 1\n",
-                   vec![])
-               )
-    );
+    #[test]
+    fn todos() {
+        let input =
+            "line 1\n\
+             TODO: todo1\n\
+             line 2\n\
+             line 3 -- TODO: todo2\n";
 
-    let input = "line1 TODO: todo1\n";
-    assert_eq!(parse_todos(input),
-               Ok(("",
-                   vec![
-                       Todo { tag: "TODO:".to_owned(),
-                              text: " todo1".to_owned()}])
-               )
-    );
+        assert_eq!(parse_todos(input),
+                   Ok(("",
+                       vec![
+                           Todo{ tag: "TODO:".to_owned(),
+                                 text: " todo1".to_owned()},
+                           Todo{ tag: "TODO:".to_owned(),
+                                 text: " todo2".to_owned()},
+                       ])
+                   )
+        );
 
-    let input =
-        "line1\n\
-         line2\n\
-         line3\n\
-         TODO: todo1\n\
-         line4\n";
-    assert_eq!(parse_todos(input),
-               Ok(("line4\n",
-                   vec![
-                       Todo { tag: "TODO:".to_owned(),
-                              text: " todo1".to_owned()}])
-               )
-    );
+        let input = "line 1\n";
+        assert_eq!(parse_todos(input),
+                   Ok(("line 1\n",
+                       vec![])
+                   )
+        );
 
-    let input =
-        "line1\n\
-         line2\n\
-         line3\n\
-         FIXME: todo1\n\
-         line4\n";
-    assert_eq!(parse_todos(input),
-               Ok((input,
-                   vec![])
-               )
-    );
+        let input = "line1 TODO: todo1\n";
+        assert_eq!(parse_todos(input),
+                   Ok(("",
+                       vec![
+                           Todo { tag: "TODO:".to_owned(),
+                                  text: " todo1".to_owned()}])
+                   )
+        );
 
-}
+        let input =
+            "line1\n\
+             line2\n\
+             line3\n\
+             TODO: todo1\n\
+             line4\n";
+        assert_eq!(parse_todos(input),
+                   Ok(("line4\n",
+                       vec![
+                           Todo { tag: "TODO:".to_owned(),
+                                  text: " todo1".to_owned()}])
+                   )
+        );
+
+        let input =
+            "line1\n\
+             line2\n\
+             line3\n\
+             FIXME: todo1\n\
+             line4\n";
+        assert_eq!(parse_todos(input),
+                   Ok((input,
+                       vec![])
+                   )
+        );
+
+    }
 
 
-#[test]
-fn todo_clean() {
-    let input = "TODO: test todo";
-    assert_eq!(
-        parse_todo("TODO:", input),
-        Ok(("",
-            Todo {
-                tag: "TODO:".to_owned(),
-                text: " test todo".to_owned()
-            })
-        )
-    );
-}
+    #[test]
+    fn todo_clean() {
+        let input = "TODO: test todo";
+        assert_eq!(
+            parse_todo("TODO:", input),
+            Ok(("",
+                Todo {
+                    tag: "TODO:".to_owned(),
+                    text: " test todo".to_owned()
+                })
+            )
+        );
+    }
 
-#[test]
-fn todo_tag_only() {
-    let input = "TODO:\nline1";
-    assert_eq!(
-        parse_todo("TODO:", input),
-        Ok(("line1",
-            Todo {
-                tag: "TODO:".to_owned(),
-                text: "".to_owned()
-            })
-        )
-    );
+    #[test]
+    fn todo_tag_only() {
+        let input = "TODO:\nline1";
+        assert_eq!(
+            parse_todo("TODO:", input),
+            Ok(("line1",
+                Todo {
+                    tag: "TODO:".to_owned(),
+                    text: "".to_owned()
+                })
+            )
+        );
 
-    let input = "TODO:";
-    assert_eq!(
-        parse_todo("TODO:", input),
-        Ok(("",
-            Todo {
-                tag: "TODO:".to_owned(),
-                text: "".to_owned()
-            })
-        )
-    );
+        let input = "TODO:";
+        assert_eq!(
+            parse_todo("TODO:", input),
+            Ok(("",
+                Todo {
+                    tag: "TODO:".to_owned(),
+                    text: "".to_owned()
+                })
+            )
+        );
 
-    let input = "TODO:\n";
-    assert_eq!(
-        parse_todo("TODO:", input),
-        Ok(("",
-            Todo {
-                tag: "TODO:".to_owned(),
-                text: "".to_owned()
-            })
-        )
-    );
-}
+        let input = "TODO:\n";
+        assert_eq!(
+            parse_todo("TODO:", input),
+            Ok(("",
+                Todo {
+                    tag: "TODO:".to_owned(),
+                    text: "".to_owned()
+                })
+            )
+        );
+    }
 
-#[test]
-fn todo_clean_newline() {
-    let input = "TODO: test todo\nline1";
-    assert_eq!(
-        parse_todo("TODO:", input),
-        Ok(("line1",
-            Todo {
-                tag: "TODO:".to_owned(),
-                text: " test todo".to_owned()
-            })
-        )
-    );
-}
+    #[test]
+    fn todo_clean_newline() {
+        let input = "TODO: test todo\nline1";
+        assert_eq!(
+            parse_todo("TODO:", input),
+            Ok(("line1",
+                Todo {
+                    tag: "TODO:".to_owned(),
+                    text: " test todo".to_owned()
+                })
+            )
+        );
+    }
 
-#[test]
-fn todo_in_comment() {
-    let input = "// TODO: test todo";
-    assert_eq!(
-        parse_todo("TODO:", input),
-        Ok(("",
-            Todo {
-                tag: "TODO:".to_owned(),
-                text: " test todo".to_owned()
-            })
-        )
-    );
-}
+    #[test]
+    fn todo_in_comment() {
+        let input = "// TODO: test todo";
+        assert_eq!(
+            parse_todo("TODO:", input),
+            Ok(("",
+                Todo {
+                    tag: "TODO:".to_owned(),
+                    text: " test todo".to_owned()
+                })
+            )
+        );
+    }
 
-#[test]
-fn todo_different_tag() {
-    let input = "FIXME: different tag";
-    assert_eq!(parse_todo("TODO:", input).ok(), None);
+    #[test]
+    fn todo_different_tag() {
+        let input = "FIXME: different tag";
+        assert_eq!(parse_todo("TODO:", input).ok(), None);
+    }
 }
